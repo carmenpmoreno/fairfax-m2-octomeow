7a8605edbd9df502e70c9db11dd2cd605b07436f
antonellam <antonella.michelli@gmail.com>
2019-03-31 22:39:11 +0200

    Collapsible form js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+//Collapsible
+
+let coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+
+  coll[i].addEventListener("click", function() {
+    let content = this.nextElementSibling;
+    
+    if (content.style.display === "flex") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "flex";
+      arrow.classList.add('fa-chevron-up');
+    }
+  });
+}
