2bda126e6cefdb536e4c0ffda4daca711b972de2
antonellam <antonella.michelli@gmail.com>
2019-04-04 00:03:13 +0200

    Name preview event function done

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,3 +5,37 @@ console.log('>> Ready :)');
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+const inputEl = document.querySelector('.fill-in__input');
+const inputDefaultText = "Nombre de prueba";
+const nameCardEl = document.querySelector('.full-name');
+
+function writeCard () {
+    console.log('Listener is working');
+    const inputText = inputEl.value || inputDefaultText;
+    nameCardEl.innerHTML =  inputText;
+};
+
+// function writeCard () {
+//     console.log('Listener is working');
+//     if (inputEl.value) {
+//     let inputText = event.currentTarget.value;
+//     nameCardEl.innerHTML =  inputText;
+//     } else {
+//     let inputText =  inputDefaultText;
+//     nameCardEl.innerHTML =  inputText;
+//     };
+// };
+
+// function writeCard () {
+//     console.log('Listener is working');
+//     if (!inputEl.value) {
+//         let inputText =  inputDefaultText;
+//         nameCardEl.innerHTML =  inputText;
+//     } else {
+//         let inputText = inputEl.value;
+//         nameCardEl.innerHTML =  inputText;
+//     };
+// };
+
+inputEl.addEventListener('keyup', writeCard);
