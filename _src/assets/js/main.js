9cfdd3250a45bfd575249e805d3be07d3f4ca0a7
younglust <younglust@gmail.com>
2019-04-04 12:22:27 +0200

    Fix consts in href gitHub

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,28 +6,16 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-//pruebas collapse
-const collapse = document.querySelector('.collapse');
-
-
-// handler
-// function addNone() {
-// }
-// collapse.addEventListener('click', showAlert);
-
-
-
 //INPUT NAME
 const previewName = document.querySelector('.full-name');
 const inputName = document.querySelector('#full_name');
 const defaultName = 'Nombre de Prueba';
 
-function fillCardInputName() {  
-  const inputNameValue = inputName.value;
-  previewName.innerHTML = inputNameValue || defaultName;
+function fillCardInputName() {
+    const inputNameValue = inputName.value;
+    previewName.innerHTML = inputNameValue || defaultName;
 }
 inputName.addEventListener('keyup', fillCardInputName);
-
 
 
 //INPUT LASTNAME
@@ -35,27 +23,21 @@ const previewJob = document.querySelector('.occupation');
 const inputJob = document.querySelector('#job');
 const defaultJob = 'SuperWoman';
 
-function fillCardInputJob() {  
-  const inputJobValue = inputJob.value;
-
-  // if (inputJobValue) {
-  //     previewJob.innerHTML = inputJobValue;
-  // } else {
-  //     previewJob.innerHTML = defaultJob;
-  // }
-  previewJob.innerHTML = inputJobValue || defaultJob;
+function fillCardInputJob() {
+    const inputJobValue = inputJob.value;
+    previewJob.innerHTML = inputJobValue || defaultJob;
 }
 inputJob.addEventListener('keyup', fillCardInputJob);
-
 
 
 //ADD link href gitHub
 const inputLink = document.getElementById('github');
 const addLink = document.getElementById('linkGithub');
+const defaultLink = 'https://github.com/';
+addLink.href = defaultLink;
 
-function addMyLink(){
-  addLink.href = inputLink.value;
+function addMyLink() {
+    addLink.href = inputLink.value || defaultLink;
 }
-
 inputLink.addEventListener('keyup', addMyLink);
 
