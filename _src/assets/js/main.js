81cfa98c2e2f196a78c5d4ae1ef4b0230f533d43
carmenpmoreno <carmenp.moreno@gmail.com>
2019-04-04 16:09:29 +0200

    add mailto

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,15 +1,14 @@
 'use strict';
 
-console.log('>> Ready :)');
-
 // Añado clase "".email" al enlace de email en _contact.html
 const previewLink = document.querySelector('.email');
 const emailInput = document.getElementById('email');
 
 // handler
 function insertEmailOnPreview () {
-    previewLink.href = emailInput.value;
+    previewLink.href = `mailto:${emailInput.value}`;
 };
 
 // listener
 emailInput.addEventListener('keyup', insertEmailOnPreview);
+previewLink.addEventListener ('mouseover', )
