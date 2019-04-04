8fcf1a74cfc90d06b8519bb5327379a63e5f80b5
carmenpmoreno <carmenp.moreno@gmail.com>
2019-04-04 10:51:42 +0200

    first steps

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
-// console.log('>> Ready :)');
+console.log('>> Ready :)');
 // // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-// let vh = window.innerHeight * 0.01;
+let vh = window.innerHeight * 0.01;
 // // Then we set the value in the --vh custom property to the root of the document
-// document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 console.log('Hola');
 
