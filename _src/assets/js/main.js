dda86e64b8052be28c18a2b9545aca6e16322a2b
patrozada <patriciarozada@gmail.com>
2019-04-04 19:52:25 +0200

    Add clean js fot job field

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,21 +1,3 @@
-'use strict';
-
-console.log("hola Pat");
-
-const fullNameEl = document.getElementById('full_name');
-const fullNamePreview = document.querySelector('.full-name');
-const defaultName = "Nombre de Prueba";
-fullNamePreview.innerHTML= defaultName;
-
-function getFullNameValue(){
-    if(fullNameEl.value){
-        fullNamePreview.innerHTML = fullNameEl.value;
-    }else{
-    fullNamePreview.innerHTML=defaultName;
-    }
-}
-fullNameEl.addEventListener('keyup', getFullNameValue);
- 
 //CAMPO JOB
 const jobEl = document.getElementById('job');
 const jobPreview = document.querySelector('.occupation');
@@ -31,22 +13,3 @@ function getJobValue(){
     }
 }
 jobEl.addEventListener('keyup', getJobValue);
-
-// Prueba con enlace LinkedIn
-
-
-const linkedInEl = document.getElementById(linkedin);
-const linkedInLink = document.getElementById('linkedin--link');
-const defaultLink = 'linkedin.com';
-function getLinkedInLink(){
-    linkedInLink.href = linkedInEl.value || defaultLink;
-}
-// function getLinkedInLink(){
-//     if (linkedInEl.value){
-//         linkedInLink.href = linkedInEl.value;
-//     }else{
-//         linkedInLink.href=defaultLink;
-//     }
-// }
-linkedInEl.addEventListener('keyup', getLinkedInLink);
-console.log(linkedInEl.value);
