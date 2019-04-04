fdc9528c18fc7e50d251963dfbacd704d188d05f
sarayfg <sarayfernandezgarcia@gmail.com>
2019-04-04 17:52:21 +0200

    improve function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,12 +1,22 @@
 'use strict';
 
 console.log('>> Ready :)');
+// change button submit color
 
 const formButton = document.querySelector('.share-button');
+const previewIcon= document.querySelector('.contact__list--rrss');
+const emptyInput= document.querySelector('.fill-in__input');
 
-function changeButtonColor(){
+function changeButtonColor(event){
+event.preventDefault();
 
 formButton.classList.add('share-button-pushed');
+
+    function changeIconColor() {
+        if (emptyInput.value === ''){
+        previewIcon.classList.add('contact__list--rrss-default');}
+    }
 }
+// añadir cambio de color en los iconos cuando su input está vacia
 
 formButton.addEventListener('click', changeButtonColor);
