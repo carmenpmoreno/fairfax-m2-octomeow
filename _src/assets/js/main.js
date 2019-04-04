df1d180e9a1f616832cdee4d361c5aaa0489268f
patrozada <patriciarozada@gmail.com>
2019-04-04 11:24:52 +0200

    Add js for job preview

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,7 +1,52 @@
 'use strict';
 
-console.log('>> Ready :)');
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+console.log("hola Pat");
+
+const fullNameEl = document.getElementById('full_name');
+const fullNamePreview = document.querySelector('.full-name');
+const defaultName = "Nombre de Prueba";
+fullNamePreview.innerHTML= defaultName;
+
+function getFullNameValue(){
+    if(fullNameEl.value){
+        fullNamePreview.innerHTML = fullNameEl.value;
+    }else{
+    fullNamePreview.innerHTML=defaultName;
+    }
+}
+fullNameEl.addEventListener('keyup', getFullNameValue);
+ 
+//CAMPO JOB
+const jobEl = document.getElementById('job');
+const jobPreview = document.querySelector('.occupation');
+const defaultJob = "Profesión";
+
+jobPreview.innerHTML = defaultJob;
+
+function getJobValue(){
+    if (jobEl.value){
+        jobPreview.innerHTML = jobEl.value;
+    }else{
+        fullNamePreview.innerHTML = defaultJob;
+    }
+}
+jobEl.addEventListener('keyup', getJobValue);
+
+// Prueba con enlace LinkedIn
+
+
+const linkedInEl = document.getElementById(linkedin);
+const linkedInLink = document.getElementById('linkedin--link');
+const defaultLink = 'linkedin.com';
+
+function getLinkedInLink(){
+    if (linkedInEl.value){
+        linkedInLink.href = linkedInEl.value;
+    }else{
+        linkedInLink.href=defaultLink;
+    }
+}
+
+
+linkedInEl.addEventListener('keyup', getLinkedInLink);
+console.log(linkedInEl.value);
