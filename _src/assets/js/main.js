87aae29dd74f4412297322903153eae760e39024
younglust <younglust@gmail.com>
2019-04-04 11:43:44 +0200

    Add gH dynamic href

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,3 +5,57 @@ console.log('>> Ready :)');
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+//pruebas collapse
+const collapse = document.querySelector('.collapse');
+
+
+// handler
+// function addNone() {
+// }
+// collapse.addEventListener('click', showAlert);
+
+
+
+//INPUT NAME
+const previewName = document.querySelector('.full-name');
+const inputName = document.querySelector('#full_name');
+const defaultName = 'Nombre de Prueba';
+
+function fillCardInputName() {  
+  const inputNameValue = inputName.value;
+  previewName.innerHTML = inputNameValue || defaultName;
+}
+inputName.addEventListener('keyup', fillCardInputName);
+
+
+
+//INPUT LASTNAME
+const previewJob = document.querySelector('.occupation');
+const inputJob = document.querySelector('#job');
+const defaultJob = 'SuperWoman';
+
+function fillCardInputJob() {  
+  const inputJobValue = inputJob.value;
+
+  // if (inputJobValue) {
+  //     previewJob.innerHTML = inputJobValue;
+  // } else {
+  //     previewJob.innerHTML = defaultJob;
+  // }
+  previewJob.innerHTML = inputJobValue || defaultJob;
+}
+inputJob.addEventListener('keyup', fillCardInputJob);
+
+
+
+//ADD link href gitHub
+const inputLink = document.getElementById('github');
+const addLink = document.getElementById('linkGithub');
+
+function addMyLink(){
+  addLink.href = inputLink.value;
+}
+
+inputLink.addEventListener('keyup', addMyLink);
+
