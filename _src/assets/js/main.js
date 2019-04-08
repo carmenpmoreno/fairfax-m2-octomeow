b93a3c75271759fe5100b7de6159476c75afad66
antonellam <antonella.michelli@gmail.com>
2019-04-08 20:29:07 +0200

    Modify tel

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -180,3 +180,16 @@ for (let i=0; i<=2; i++){
 
 paletteIds[i].addEventListener('click', changeColor);
 }
+
+
+const inputTelEl =  document.querySelector('.input__tel');
+const iconMobEl = document.querySelector('.icon__link--mobile');
+
+function insertHref () {
+    console.log('Start insertHref');
+    const inputTelText = inputTelEl.value;
+    iconMobEl.href = `tel:${inputTelText}`;
+    iconMobEl.title = `${inputTelText}`;
+}
+
+inputTelEl.addEventListener('change', insertHref);
