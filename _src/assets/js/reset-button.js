af3c59fb10599ca04bdf64c4ed903c2c03947d8b
younglust <younglust@gmail.com>
2019-04-11 22:26:41 +0200

    cmonetado js

--- _src/assets/js/reset-button.js
+++ _src/assets/js/reset-button.js
@@ -2,15 +2,58 @@ const resetButon = document.querySelector('.reset--button');
 const defaultcolor = document.querySelector('.default--palette');
 const inputResetEl = document.querySelectorAll('.fill-in__input');
 
-// resetButon.onclick = function () {
-//     inputMain.value = '';
-//     parrafMain.innerHTML = defaultText;
-//     counter.innerHTML = 0;
-// }
-function resetFormColors() {
-    inputResetEl[i].value = '';
+for (let i = 0; i < inputResetEl.length; i++) {
+    const everyResetInput = inputResetEl[i];
 
-    console.log('click');
+    resetButon.addEventListener('click', function resetFormColors() {
+        inputResetEl[i].value = '';
+    });
+};
+
+
+/*
+
+//------------------
+//prueba change color
+//Resumen: cambiar color del preview cuando se selecciones los radiobutton
+
+//Recoger todos los radiobutton
+const inputsRadio = document.querySelectorAll('.radio');
+
+//Recoger el preview
+const preview = document.getElementById('preview');
+
+//Crear la funcion y el listener del radiobutton
+//Se recorre cada input con el bucle
+for (let i = 0; i < inputsRadio.length; i++) {
+
+    //asignamos a la constante el valor de cada value
+    const everyInput = inputsRadio[i];
+
+    everyInput.addEventListener('click', function () {
+
+        //asignamos a la constante el valor de cada radio button
+        const radioValue = everyInput.value;
+
+        //Añadir o quitar las clases en el preview según el radiobutton.
+        // Por defecto verde
+
+        //Si el value = a rojo, añadir clase roja y quitar las otras
+        if (radioValue === 'red') {
+            preview.classList.add('preview-red');
+            preview.classList.remove('preview-blue', 'preview-green');
+
+            //Si el value = a azul, añadir clase roja y quitar las otras
+        } else if (radioValue === 'blue') {
+            preview.classList.add('preview-blue');
+            preview.classList.remove('preview-green', 'preview-red');
+
+            //añadir clase green y quitar las otras
+        } else {
+            preview.classList.add('preview-green');
+            preview.classList.remove('preview-blue', 'preview-red');
+        }
+
+    });
 }
-
-resetButon.addEventListener('click', resetFormColors);
+*/
