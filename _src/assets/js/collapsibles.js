37e224e196aa6906898371fb451398f09b1169ec
carmenpmoreno <carmenp.moreno@gmail.com>
2019-04-15 20:06:10 +0200

    try to close others collapsibles

--- _src/assets/js/collapsibles.js
+++ _src/assets/js/collapsibles.js
@@ -13,16 +13,17 @@ for (const legend of legends) {
 // hacer un handler para que al hacer click en el legend "x" nos muestre su clase collapsible, pero ocultando la de los demás legend
     function handlebuttonclick(event) {
         console.log('hola');
-        // si se abre un contenedor, se añade una clase con la flecha invertida
-
-        // y se cierran los otros contenedores si estaban abiertos
-
+        // bucle para que estén todos los collapsibles cerrados
+        for (const collapsible of collapsibles) {
+            collapsible.classList.add('hidden');
+        }
+        
 
         // si está cerrado, se abre y si está abierto, se cierra
         if (event.currentTarget.classList.contains('closed')) {
             event.currentTarget.nextElementSibling.classList.remove('hidden');
             event.currentTarget.classList.remove('closed');
-            event.currentTarget.classList.add('open-arrow ');
+            event.currentTarget.classList.add('open-arrow');
         }
         else {
             event.currentTarget.nextElementSibling.classList.add('hidden');
