62cb61cf6a1eeadcb279bd89edee8c816905402e
carmenpmoreno <carmenp.moreno@gmail.com>
2019-04-15 21:02:18 +0200

    final collapsibles closed

--- _src/assets/js/collapsibles.js
+++ _src/assets/js/collapsibles.js
@@ -2,35 +2,34 @@
 
 // Recoger la clase "legend" con QuerySelectoAll
 const legends = document.querySelectorAll('.legend');
+console.log(legends);
 // Recogemos la clase homónima "collapsible"
-const collapsibles = document.querySelectorAll('collapsibles');
-
+const collapsibles = document.querySelectorAll('.collapsibles');
+console.log(collapsibles);
 // listener sobre el legend
 for (const legend of legends) {
     legend.addEventListener('click', handlebuttonclick);
 }
 
 // hacer un handler para que al hacer click en el legend "x" nos muestre su clase collapsible, pero ocultando la de los demás legend
-    function handlebuttonclick(event) {
-        console.log('hola');
-        // bucle para que estén todos los collapsibles cerrados
-        for (const collapsible of collapsibles) {
-            collapsible.classList.add('hidden');
+function handlebuttonclick(event) {
+    console.log('hola');
+    // si está cerrado, se abre y quito el indicador de cerrado en el legend
+    // TO DO: en clase closed, poner la flecha en el otro sentido.
+    if (event.currentTarget.classList.contains('closed')) {
+        for (let i=0; i<legends.length; i++) {
+            collapsibles[i].classList.add('hidden');
+            legends[i].classList.add('closed');
         }
-        
-
-        // si está cerrado, se abre y si está abierto, se cierra
-        if (event.currentTarget.classList.contains('closed')) {
-            event.currentTarget.nextElementSibling.classList.remove('hidden');
-            event.currentTarget.classList.remove('closed');
-            event.currentTarget.classList.add('open-arrow');
-        }
-        else {
-            event.currentTarget.nextElementSibling.classList.add('hidden');
-            event.currentTarget.classList.add('closed');
-        }
-        
+        event.currentTarget.nextElementSibling.classList.remove('hidden');
+        event.currentTarget.classList.remove('closed');
     }
+    else {
+        event.currentTarget.nextElementSibling.classList.add('hidden');
+        event.currentTarget.classList.add('closed');
+    }
+
+}
 
 
 
