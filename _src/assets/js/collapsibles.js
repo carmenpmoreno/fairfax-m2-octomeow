022fe5414d8de87a2ca3629e90fd62ef0176e72b
antonellam <antonella.michelli@gmail.com>
2019-04-15 19:55:31 +0200

    Commit

--- _src/assets/js/collapsibles.js
+++ _src/assets/js/collapsibles.js
@@ -1,3 +1,4 @@
+/* eslint-disable strict */
 // COLLAPSIBLES
 
 const legendOneEl = document.querySelector('.legend1');
@@ -11,45 +12,45 @@ const arrowTwoEl = document.querySelector('.arrow2');
 const arrowThreeEl = document.querySelector('.arrow3');
 
 function legendOneClickHandler() {
-    console.log('Listener 1 works');
-    collapsibleTwoEl.classList.remove('fill-in__items');
-    collapsibleThreeEl.classList.remove('share__container');
-    arrowTwoEl.classList.remove('fa-chevron-up');
-    arrowThreeEl.classList.remove('fa-chevron-up');
-    collapsibleOneEl.classList.toggle('option__distribution');
-    if (collapsibleOneEl.classList.contains('option__distribution')) {
-        arrowOneEl.classList.add('fa-chevron-up');
-    } else {
-        arrowOneEl.classList.remove('fa-chevron-up');
-    };
+  console.log('Listener 1 works');
+  collapsibleTwoEl.classList.remove('fill-in__items');
+  collapsibleThreeEl.classList.remove('share__container');
+  arrowTwoEl.classList.remove('fa-chevron-up');
+  arrowThreeEl.classList.remove('fa-chevron-up');
+  collapsibleOneEl.classList.toggle('option__distribution');
+  if (collapsibleOneEl.classList.contains('option__distribution')) {
+    arrowOneEl.classList.add('fa-chevron-up');
+  } else {
+    arrowOneEl.classList.remove('fa-chevron-up');
+  }
 }
 
 function legendTwoClickHandler() {
-    console.log('Listener 2 works');
-    collapsibleOneEl.classList.remove('option__distribution');
-    collapsibleThreeEl.classList.remove('share__container');
-    arrowOneEl.classList.remove('fa-chevron-up');
-    arrowThreeEl.classList.remove('fa-chevron-up');
-    collapsibleTwoEl.classList.toggle('fill-in__items');
-    if (collapsibleTwoEl.classList.contains('fill-in__items')) {
-        arrowTwoEl.classList.add('fa-chevron-up');
-    } else {
-        arrowTwoEl.classList.remove('fa-chevron-up');
-    };
+  console.log('Listener 2 works');
+  collapsibleOneEl.classList.remove('option__distribution');
+  collapsibleThreeEl.classList.remove('share__container');
+  arrowOneEl.classList.remove('fa-chevron-up');
+  arrowThreeEl.classList.remove('fa-chevron-up');
+  collapsibleTwoEl.classList.toggle('fill-in__items');
+  if (collapsibleTwoEl.classList.contains('fill-in__items')) {
+    arrowTwoEl.classList.add('fa-chevron-up');
+  } else {
+    arrowTwoEl.classList.remove('fa-chevron-up');
+  }
 }
 
 function legendThreeClickHandler() {
-    console.log('Listener 3 works');
-    collapsibleOneEl.classList.remove('option__distribution');
-    collapsibleTwoEl.classList.remove('fill-in__items');
-    arrowOneEl.classList.remove('fa-chevron-up');
-    arrowTwoEl.classList.remove('fa-chevron-up');
-    collapsibleThreeEl.classList.toggle('share__container');
-    if (collapsibleThreeEl.classList.contains('share__container')) {
-        arrowThreeEl.classList.add('fa-chevron-up');
-    } else {
-        arrowThreeEl.classList.remove('fa-chevron-up');
-    };
+  console.log('Listener 3 works');
+  collapsibleOneEl.classList.remove('option__distribution');
+  collapsibleTwoEl.classList.remove('fill-in__items');
+  arrowOneEl.classList.remove('fa-chevron-up');
+  arrowTwoEl.classList.remove('fa-chevron-up');
+  collapsibleThreeEl.classList.toggle('share__container');
+  if (collapsibleThreeEl.classList.contains('share__container')) {
+    arrowThreeEl.classList.add('fa-chevron-up');
+  } else {
+    arrowThreeEl.classList.remove('fa-chevron-up');
+  }
 }
 
 legendOneEl.addEventListener('click', legendOneClickHandler);
--- _src/assets/js/object.js
+++ _src/assets/js/object.js
@@ -1,51 +1,69 @@
+/* eslint-disable no-console */
+/* eslint-disable strict */
 console.log('Hola');
 
 let card = {
-    name: 'Nombre y apellido',
-    job: 'Trabajo',
-    phone: '',
-    email: '',
-    linkedin: '',
-    github: '',
-    photo: '',
-    palette: '1',
+  name: 'Nombre y apellido',
+  job: 'Trabajo',
+  phone: '',
+  email: '',
+  linkedin: '',
+  github: '',
+  photo: '',
+  palette: '1',
 };
 
 let localStorageKey = 'cacheCard';
 const inputUpdateEls = document.querySelectorAll('.input-update');
 console.log(inputUpdateEls);
 
+
 let cacheCard = () => {
-    console.log('Odio la puta caché');
-    if (localStorage.cacheCard) {
-        const savedCard = JSON.parse(localStorage.getItem('cacheCard'));
-        console.log(savedCard);
-    } 
-}
+  console.log('Odio la puta caché');
+  if (localStorage.cacheCard) {
+    const savedCard = JSON.parse(localStorage.getItem('cacheCard'));
+    console.log(savedCard);
+    card.name = savedCard.name;
+    card.job = savedCard.job;
+    card.phone = savedCard.phone;
+    card.email = savedCard.email;
+    card.linkedin = savedCard.linkedin;
+    card.github = savedCard.github;
+    card.photo = savedCard.photo;
+    card.palette = savedCard.palette;
+    console.log(card);
+  }
+};
 
 cacheCard();
 
+let fillCardWithCache = () => {
+  console.log('card para rellenar', card);
+  for (const entry of inputUpdateEls.entries()) {
+    console.log(entry[1].value); 
+  }
+};
+
+fillCardWithCache();
 //HACER FUNCIÓN QUE DESDE SAVEDCARD RELLENE LOS INPUTS
 //EL RESET VOLVERÁ EL OBJETO AL ESTADO DE CARD INICIAL
 
 function cardUpdate(name, value) {
-    card[name] = value;
-}
-
-function inputChangeHandler(event) {
-    const currentInput = event.currentTarget;
-    const name = currentInput.name;
-    const value = currentInput.value;
-    cardUpdate(name, value);
-    finalFormHandler();
-}
-
-for (let i = 0; i < inputUpdateEls.length; i++) {
-    inputUpdateEls[i].addEventListener('change', inputChangeHandler);
+  card[name] = value;
 }
 
 function finalFormHandler() {
-    localStorage.setItem(localStorageKey, JSON.stringify(card));
+  localStorage.setItem(localStorageKey, JSON.stringify(card));
 }
 
-// formButton.addEventListener('click', finalFormHandler);
+function inputChangeHandler(event) {
+  const currentInput = event.currentTarget;
+  const name = currentInput.name;
+  const value = currentInput.value;
+  cardUpdate(name, value);
+  finalFormHandler();
+}
+
+for (let i = 0; i < inputUpdateEls.length; i++) {
+  inputUpdateEls[i].addEventListener('change', inputChangeHandler);
+}
