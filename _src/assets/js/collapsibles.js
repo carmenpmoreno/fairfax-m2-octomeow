134dafc29967a4692246d65f3cbcda45fbe83683
antonellam <antonella.michelli@gmail.com>
2019-04-16 20:11:57 +0200

    Fix merge

--- _src/assets/js/collapsibles.js
+++ _src/assets/js/collapsibles.js
@@ -3,10 +3,8 @@
 
 // Recoger la clase "legend" con QuerySelectoAll
 const legends = document.querySelectorAll('.legend');
-console.log(legends);
 // Recogemos la clase homónima "collapsible"
 const collapsibles = document.querySelectorAll('.collapsibles');
-console.log(collapsibles);
 // listener sobre el legend
 for (const legend of legends) {
   legend.addEventListener('click', handlebuttonclick);
@@ -14,7 +12,6 @@ for (const legend of legends) {
 
 // hacer un handler para que al hacer click en el legend "x" nos muestre su clase collapsible, pero ocultando la de los demás legend
 function handlebuttonclick(event) {
-  console.log('hola');
   // si está cerrado, se abre y quito el indicador de cerrado en el legend
   // TO DO: en clase closed, poner la flecha en el otro sentido.
   if (event.currentTarget.classList.contains('closed')) {
