be8cc1c648402e57250cb1ae9ba73f8852e9e211
younglust <younglust@gmail.com>
2019-04-16 11:35:51 +0200

    fix code reset

--- _src/assets/js/reset-button.js
+++ _src/assets/js/reset-button.js
@@ -5,22 +5,28 @@ const liPreviewEl = document.querySelectorAll(".contact__list--rrss");
 const cardPreviewElClases = ["palette--1", "palette--2", "palette--3"];
 const backToDefault = "";
 
-for (let i = 0; i < inputResetEl.length; i++) {
-  for (let j = 0; j < liPreviewEl.length; j++) {
-    for (let k = 0; k < cardPreviewElClases.length; k++) {
-      resetButon.addEventListener("click", function resetFormColors() {
-        inputResetEl[i].value = backToDefault;
-        //preview
-        nameCardEl.innerHTML = inputDefaultText;
-        jobPreview.innerHTML = defaultJob;
-        liPreviewEl[j].children[0].href = backToDefault;
-        iconMobEl.title = backToDefault;
-        //colors
-        if (cardPreviewEl.classList.contains(cardPreviewElClases[k])) {
-          cardPreviewEl.classList.remove(cardPreviewElClases[k]);
-          cardPreviewEl.classList.add("default--palette");
-        }
-      });
+function loopColors() {
+  for (let k = 0; k < cardPreviewElClases.length; k++) {
+    if (cardPreviewEl.classList.contains(cardPreviewElClases[k])) {
+      cardPreviewEl.classList.remove(cardPreviewElClases[k]);
+      cardPreviewEl.classList.add("default--palette");
     }
   }
+}
+function loopPreview() {
+  for (let j = 0; j < liPreviewEl.length; j++) {
+    nameCardEl.innerHTML = inputDefaultText;
+    jobPreview.innerHTML = defaultJob;
+    liPreviewEl[j].children[0].href = backToDefault;
+    iconMobEl.title = backToDefault;
+  }
+}
+for (let i = 0; i < inputResetEl.length; i++) {
+  resetButon.addEventListener("click", function () {
+    inputResetEl[i].value = backToDefault;
+    //preview
+    loopPreview();
+    //colors
+    loopColors();
+  });
 }
