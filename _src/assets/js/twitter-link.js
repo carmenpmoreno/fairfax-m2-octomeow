0e166b97c473fef576d362c9a9291213b4fa69a5
sarayfg <sarayfernandezgarcia@gmail.com>
2019-04-17 20:27:03 +0200

    add link to twitter

--- _src/assets/js/twitter-link.js
+++ _src/assets/js/twitter-link.js
@@ -10,18 +10,18 @@ Compartir en Twitter
 
 // si queremos que se añada un texto despues de ? escribimos text= para una url &url=
 
-
-const twitterText = `Esta es mi nueva tarjeta de contacto: LINK Si quieres una de nuestras Awesome Profile Cards, visita: http://beta.adalab.es/fairfax-m2-octomeow/`;
-//const cardTwitterLink=;
-const linkTextTrans = encodeURIComponent(twitterText);
-const twitterLink ='https://twitter.com/intent/tweet?';
-
-const twitterFull= `${twitterLink}`+`text=`+`${linkTextTrans}`;//+ `&url=${linkUrlTransform}`;
-
-console.log(twitterFull);
-
-const shareLink = document.querySelector('.twitter-link');
-shareLink.href= twitterFull;
+function callTwitter (){
+    const twitterText = `Esta es mi nueva tarjeta de contacto. Si quieres una de nuestras Awesome Profile Cards, visita: http://beta.adalab.es/fairfax-m2-octomeow/`;
+    //const cardTwitterLink=;
+    const linkTextTrans = encodeURIComponent(twitterText);
+    const linkCard = finalUrl;
+    const linkCardTransform = encodeURIComponent(linkCard);
+    const twitterLink ='https://twitter.com/intent/tweet?';
+    const twitterFull= `${twitterLink}`+`text=`+`${linkTextTrans}`+ `&url=`+`${linkCardTransform}`;
+    console.log(twitterFull);
+    const shareLink = document.querySelector('.twitter-link');
+    shareLink.href= twitterFull;
+}
 
 console.log(shareLink);
 
@@ -29,16 +29,18 @@ console.log(shareLink);
 // const twitterSendButton = document.querySelector('.twitter-link');
 
 // function twitterButtonHandler(event) {
-// //   event.preventDefault;
-//   const finalUrlEl = document.querySelector('.twitter-link.result');
-//   console.log(finalUrl);
+//   event.preventDefault;
+//   const finalUrlEl = document.querySelector('.twitter-link-result');
+ 
 //   const finalUrl = finalUrlEl.innerHTML;
-//   const twitterText = `Esta es mi nueva tarjeta de contacto: ${finalUrl} Si quieres una de nuestras Awesome Profile Cards, visita: http://beta.adalab.es/fairfax-m2-octomeow/`;
+//   console.log(finalUrl);
+//   const twitterText = `Esta es mi nueva tarjeta de contacto: ${linkUrlTransform} Si quieres una de nuestras Awesome Profile Cards, visita: http://beta.adalab.es/fairfax-m2-octomeow/`;
 //   //const cardTwitterLink=;
 //   const linkTextTrans = encodeURIComponent(twitterText);
+//   const linkUrlTransform = ncodeURIComponent(finalUrl);
 //   const twitterLink ='https://twitter.com/intent/tweet?';
 
-//   const twitterFull= `${twitterLink}`+`text=`+`${linkTextTrans}`;//+ `&url=${linkUrlTransform}`;
+//   const twitterFull= `${twitterLink}`+`text=`+`${linkTextTrans}`//+`&url=` + `${linkUrlTransform}`
 
 //   console.log(twitterFull);
 
