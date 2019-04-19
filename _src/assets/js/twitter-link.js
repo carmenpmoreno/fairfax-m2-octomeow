e6b09a2340da136206b900f511d660393b6dbdbd
antonellam <antonella.michelli@gmail.com>
2019-04-19 22:46:06 +0200

    Twitter update

--- _src/assets/js/twitter-link.js
+++ _src/assets/js/twitter-link.js
@@ -1,23 +1,11 @@
 /* eslint-disable strict */
-/* <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a>
-<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> */
-
-/* <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">
-<i class="fab fa-twitter twitter-icon"></i>
-Compartir en Twitter
-</a> */
-
-
-
-// si queremos que se añada un texto despues de ? escribimos text= para una url &url=
-const twitterText='hola que tal';
-//const cardTwitterLink=;
-const linkTextTrans= encodeURIComponent(twitterText);
-const twitterLink ='https://twitter.com/intent/tweet?';
-
-const twitterFull= `${twitterLink}`+`text=`+`${linkTextTrans}`;//+ `&url=${linkUrlTransform}`;
-
-
-const shareLink = document.querySelector('.twitter-link');
-shareLink.href= twitterFull;
-
+function callTwitter (finalUrl) {
+  const twitterText = `Esta es mi nueva tarjeta de contacto:`;
+  //const cardTwitterLink=;
+  const linkTextTrans = encodeURIComponent(twitterText);
+  const linkCardTransform = encodeURIComponent(finalUrl);
+  const twitterLink ='https://twitter.com/intent/tweet?';
+  const twitterFull= `${twitterLink}`+`text=`+`${linkTextTrans}`+ `&url=`+`${linkCardTransform}`;
+  const shareLink = document.querySelector('.twitter-link');
+  shareLink.href= twitterFull;
+}
