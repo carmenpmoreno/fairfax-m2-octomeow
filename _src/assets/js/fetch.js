1ec34e0143fff3479d4c1645a066f8e01b1bc49c
irenegf83 <irene@irenegf.com>
2019-04-30 20:32:45 +0200

    Refactorize preview linkedin and fetch

--- _src/assets/js/fetch.js
+++ _src/assets/js/fetch.js
@@ -1,32 +1,35 @@
 /* eslint-disable no-undef */
 /* eslint-disable strict */
-var form = document.querySelector('form');
+const form = document.querySelector('form');
 const twitterLinkEl = document.querySelector('.title-twitter-content');
 
-
-function sendData () {
-  var inputs = Array.from(form.elements);
-  var json = getJSONFromInputs(inputs);
-  json.skills = ['JavaScript', 'React'];
-  json.photo = fr.result;
-  sendRequest(json);
-}
-
-function loadPhoto(){
-  var myFile = document.querySelector('#img-selector').files[0];
-  fr.addEventListener('load', sendData);
-  fr.readAsDataURL(myFile);
-}
-
-function getJSONFromInputs(inputs){
-  return inputs.reduce(function (acc, val) {
-    if(val.nodeName !== 'BUTTON')
+const getJSONFromInputs = inputs => {
+  return inputs.reduce((acc, val) => {
+    if(val.nodeName !== 'BUTTON') {
       acc[val.name] = val.value;
+    }
     return acc;
   }, {});
-}
+};
 
-function sendRequest(obj){
+const showURL = result => {
+  if(result.success){
+    const linkTwitter = document.createElement('a');
+    linkTwitter.classList.add('twitter-link-result');
+    linkTwitter.href = result.cardURL;
+
+    const linkText = document.createTextNode(result.cardURL);
+    linkTwitter.appendChild(linkText);
+
+    twitterLinkEl.appendChild(linkTwitter);
+    callTwitter(result.cardURL);
+  }else{
+    const textError = document.createTextNode(`ERROR: ${result.error}`);
+    twitterLinkEl.appendChild(textError);
+  }
+};
+
+const sendRequest = obj => {
   const json = JSON.stringify(obj);
   fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
     method: 'POST',
@@ -35,18 +38,23 @@ function sendRequest(obj){
       'content-type': 'application/json'
     },
   })
-    .then(function(resp) { return resp.json(); })
-    .then(function(result) { showURL(result); })
-    .catch(function(error) { console.log(error); });
-}
+    .then(resp => resp.json())
+    .then(result => showURL(result))
+    .catch(error => console.log(error));
+};
 
-function showURL(result){
-  if(result.success){
-    twitterLinkEl.innerHTML = '<a class="twitter-link-result" href=' + result.cardURL + '>' + result.cardURL + '</a>';
-    callTwitter(result.cardURL);
-  }else{
-    twitterLinkEl.innerHTML = 'ERROR:' + result.error;
-  }
-}
+const sendData = () => {
+  const inputs = Array.from(form.elements);
+  const json = getJSONFromInputs(inputs);
+  json.skills = ['JavaScript', 'React'];
+  json.photo = fr.result;
+  sendRequest(json);
+};
+
+const loadPhoto = () => {
+  const myFile = document.querySelector('#img-selector').files[0];
+  fr.addEventListener('load', sendData);
+  fr.readAsDataURL(myFile);
+};
 
 formButton.addEventListener('click', loadPhoto);
