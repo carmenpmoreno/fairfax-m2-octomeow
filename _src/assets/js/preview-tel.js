c04893fbd115f643cd59ebb35ecc08780579a8f5
libertadpozos <libertad.pozos@gmail.com>
2019-04-30 20:15:52 +0200

    Change to arrow function

--- _src/assets/js/preview-tel.js
+++ _src/assets/js/preview-tel.js
@@ -4,10 +4,12 @@
 const inputTelEl =  document.querySelector('.input__tel');
 const iconMobEl = document.querySelector('.icon__link--mobile');
 
-function insertHref () {
+
+const insertHref = () => { 
   const inputTelText = inputTelEl.value;
   iconMobEl.href = `tel:${inputTelText}`;
   iconMobEl.title = `${inputTelText}`;
 }
 
 inputTelEl.addEventListener('change', insertHref);
+
