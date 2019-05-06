fbd318ab0001d0319fce8b737c567678074342d5
irenegf83 <irene@irenegf.com>
2019-05-06 20:23:01 +0200

    Refactorize share button

--- _src/assets/js/share.js
+++ _src/assets/js/share.js
@@ -6,11 +6,11 @@ const previewIco = document.querySelector('.contact__list--rrss');
 const emptyInput = document.querySelector('.fill-in__input');
 const twitterEl = document.querySelector('.section__twitter');
 
-function handlerSubmitButton(event){
+const handlerSubmitButton = event => {
   event.preventDefault();
 
   formButton.classList.add('share-button-pushed');
   twitterEl.classList.remove('collapsible__hidden');
-}
+};
 
 formButton.addEventListener('click', handlerSubmitButton);
