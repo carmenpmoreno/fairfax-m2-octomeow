e5a234739ee2e485ff869c24565294a4795be575
libertadpozos <libertad.pozos@gmail.com>
2019-05-06 20:25:00 +0200

    refactorize object

--- _src/assets/js/preview-tel.js
+++ _src/assets/js/preview-tel.js
@@ -1,15 +1,10 @@
-/* eslint-disable strict */
-//PHONE
-
 const inputTelEl =  document.querySelector('.input__tel');
 const iconMobEl = document.querySelector('.icon__link--mobile');
-
 
 const insertHref = () => { 
   const inputTelText = inputTelEl.value;
   iconMobEl.href = `tel:${inputTelText}`;
   iconMobEl.title = `${inputTelText}`;
 }
-
 inputTelEl.addEventListener('change', insertHref);
 
