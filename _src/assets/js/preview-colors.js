a5d28acdb39cc5b956599ffa21406f5a707a10fa
younglust <younglust@gmail.com>
2019-05-06 20:52:37 +0200

    Refactor color

--- _src/assets/js/preview-colors.js
+++ _src/assets/js/preview-colors.js
@@ -1,29 +1,29 @@
-/* eslint-disable no-inner-declarations */
 /* eslint-disable strict */
-//Color select
-
 const cardPreviewEl = document.querySelector('.card-preview');
 const palettePickerEl = document.querySelector('.option__container');
-const paletteIds =[];
+const paletteIds = [];
 const paletteClasses = [];
+const secondOption = 2;
+const thirdOption = 3;
 
-for (let i=1; i<=3; i++){
+for (let i = 1; i <= thirdOption; i++) {
   paletteIds.push(palettePickerEl.querySelector(`#p${i}`));
-  paletteClasses.push(palettePickerEl.querySelector(`.palette--${i}`).className);
+  paletteClasses.push(
+    palettePickerEl.querySelector(`.palette--${i}`).className
+  );
 }
 
-for (let i=0; i<=2; i++){
-
-  function changeColor(event){
-    const selectedItem = (event.currentTarget);
-    const selectedItemIndex =paletteIds.indexOf(selectedItem);
+for (let i = 0; i <= secondOption; i++) {
+  const changeColor = event => {
+    const selectedItem = event.currentTarget;
+    const selectedItemIndex = paletteIds.indexOf(selectedItem);
     const selectedClass = paletteClasses[selectedItemIndex];
     cardPreviewEl.classList.remove('default--palette');
-    for(let i = 0; i<=2; i++){
+
+    for (let i = 0; i <= secondOption; i++) {
       cardPreviewEl.classList.remove(`${paletteClasses[i]}`);
     }
     cardPreviewEl.classList.add(`${selectedClass}`);
-  }
-
+  };
   paletteIds[i].addEventListener('click', changeColor);
 }
