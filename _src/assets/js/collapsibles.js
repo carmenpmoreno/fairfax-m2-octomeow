73ad84fa538b2e52ba275f16154bb33495929a21
younglust <younglust@gmail.com>
2019-05-06 20:27:30 +0200

    Changes to arrow funtion

--- _src/assets/js/collapsibles.js
+++ _src/assets/js/collapsibles.js
@@ -1,24 +1,25 @@
-const legends = document.querySelectorAll(".legend");
-const collapsibles = document.querySelectorAll(".collapsibles");
+/* eslint-disable strict */
+const legends = document.querySelectorAll('.legend');
+const collapsibles = document.querySelectorAll('.collapsibles');
 
-for (const legend of legends) {
-  legend.addEventListener("click", handlebuttonclick);
-}
-
-function handlebuttonclick(event) {
+const handlebuttonclick = event => {
   const myItem = event.currentTarget;
   const siblingClass = myItem.nextElementSibling.classList;
   const itemClass = myItem.classList;
 
-  if (myItem.classList.contains("closed")) {
+  if (myItem.classList.contains('closed')) {
     for (let i = 0; i < legends.length; i++) {
-      collapsibles[i].classList.add("hidden");
-      legends[i].classList.add("closed");
+      collapsibles[i].classList.add('hidden');
+      legends[i].classList.add('closed');
     }
-    siblingClass.remove("hidden");
-    itemClass.remove("closed");
+    siblingClass.remove('hidden');
+    itemClass.remove('closed');
   } else {
-    siblingClass.add("hidden");
-    itemClass.add("closed");
+    siblingClass.add('hidden');
+    itemClass.add('closed');
   }
+};
+
+for (const legend of legends) {
+  legend.addEventListener('click', handlebuttonclick);
 }
